18a1a235f7a8ffdfc90d6ee833f100d260fb0327
hieulele <79618870+hieulele@users.noreply.github.com>
2022-12-20 00:04:59 +0700

    hieu sua frontend2

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -50,12 +50,12 @@ const FEATURE_LIST = [
     subTitle: 'Chi tiết nội dung truyện',
     to: ROUTES.STORY,
   }, */
-  {
+  /* {
     title: 'Từ điển trong Hama',
     subTitle: 'Danh sách từ vựng được phân loại theo cấp độ, loại từ, ...',
     imgUrl: dictionaryIcon,
     to: ROUTES.DYNO_DICTIONARY,
-  },
+  }, */
   /* {
     title: 'Từ vựng TOEIC',
     subTitle: 'Các từ vựng thường gặp trong đề thi Toeic',
